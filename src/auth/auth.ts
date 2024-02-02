387428fc209ccd5e1171a889b98830bf7b18eff4
Harish Raja G <harishraja411@gmail.com>
2024-02-02 13:03:55 +0530

    auth-lst

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,23 +1,23 @@
-import { validateToken } from "./validatetoken.js"
+import { validateToken } from "./validatetoken.js";
 
 let authVerify = async (request, reply, next) => {
-    // console.log(request.body.token)
-    try {
-        let result =await validateToken(request.body.token)
-        console.log(result)
-        if (result) {
-            console.log('success data')
-            next()
-        }
-        else {
-            reply.send("validaton failed")
-        }
-
-    } catch (error) {
-        console.log('error is')
-        reply.send(error.message)
+  console.log(request.body);
+  console.log("*****");
+  console.log(request.headers["Authorization"], "header sections");
+  try {
+    let result = await validateToken(request.body);
+    console.log(result, "testing data set");
+    if (result.status === true) {
+      console.log("success data");
+      console.log(result, "sucees data 2");
+      next();
+    } else {
+      reply.send("validaton failed");
     }
-}
+  } catch (error) {
+    console.log("error is");
+    reply.send(error.message);
+  }
+};
 
-
-export default authVerify
+export default authVerify;
