d1d4ee98b193c934308b6039b824240a2265142c
PravinRaja22 <102848025+PravinRaja22@users.noreply.github.com>
2024-02-08 11:21:20 +0530

    push

--- src/new.ts
+++ src/new.ts
@@ -1,14 +1,17 @@
 import fastify from "fastify";
 import oauthPlugin from "@fastify/oauth2";
 import cors from "@fastify/cors";
-import fastifyStatic from "@fastify/static";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
+import fastifyStatic from '@fastify/static';
+import { fileURLToPath } from 'url';
+import { dirname, join,resolve  } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-console.log("test");
-console.log(__dirname, "Directot name");
+const parentDir = resolve(__dirname, '..');
+
+console.log(__filename ,'fileName')
+console.log(parentDir ,'Dir Name')
+console.log(join(parentDir, "uploads") ,'join  Name')
 // import { connection } from "./database/postgress.js";
 import Routes from "./routes/HRMS/routeshrms.js";
 import path from "path";
@@ -18,24 +21,23 @@ const server = fastify({
 await server.register(cors);
 server.register(Routes);
 server.register(fastifyStatic, {
-  root: path.join(__dirname, "uploads"),
-  prefix: "/paySlip",
+  root: join(parentDir, "uploads"),
 });
 
-server.register(oauthPlugin, {
-  name: "msOAuth2",
-  credentials: {
-    client: {
-      id: "ea82ec5f-8eed-4bcd-a8a9-a23281ed90df",
-      secret: "67030a96-ed24-4883-bc54-ec3698cdc7ff",
-    },
-    auth: oauthPlugin.MICROSOFT_CONFIGURATION,
-  },
-  // register a fastify url to start the redirect flow
-  startRedirectPath: "/login/facebook",
-  // facebook redirect here after the user login
-  callbackUri: "http://localhost:3000/login/facebook/callback",
-});
+// server.register(oauthPlugin, {
+//   name: "msOAuth2",
+//   credentials: {
+//     client: {
+//       id: "ea82ec5f-8eed-4bcd-a8a9-a23281ed90df",
+//       secret: "67030a96-ed24-4883-bc54-ec3698cdc7ff",
+//     },
+//     auth: oauthPlugin.MICROSOFT_CONFIGURATION,
+//   },
+//   // register a fastify url to start the redirect flow
+//   startRedirectPath: "/login/facebook",
+//   // facebook redirect here after the user login
+//   callbackUri: "http://localhost:3000/login/facebook/callback",
+// });
 // let postgressconnection = connection();
 // console.log(postgressconnection)
 server.get("/login/ms/callback", async function (request, reply) {
