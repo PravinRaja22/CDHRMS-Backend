b140563561888b907cda7474d9b437cb5cff4aaf
Harish Raja G <harishraja411@gmail.com>
2024-02-20 18:16:11 +0530

    Update postgress.ts

--- src/database/postgress.ts
+++ src/database/postgress.ts
@@ -18,16 +18,7 @@ const pool = new pkg.Pool({
   password: "Maples71",
   host: "5.189.159.233",
   port: 65432,
-<<<<<<< Updated upstream
   database: "hrms",
-=======
-  database: "hrms"
-  // user: "postgres",
-  // password: "admin",
-  // host: "192.168.0.127",
-  // port: 5432,
-  // database: "HRMS",
->>>>>>> Stashed changes
 });
 pool.on("connect", () => {
   console.log("Database connected");
@@ -37,17 +28,15 @@ pool.on("error", (err) => {
   console.error("Error connecting to the database:", err.message);
 });
 export const query = async (stmt, options) => {
-  console.log('querying');
+  console.log("querying");
   console.log(options);
   if (Object.keys(options).length > 0 || options.length > 0) {
-    console.log('if');
+    console.log("if");
     return await pool.query(stmt, options);
-  }
-  else {
-    console.log('else');
+  } else {
+    console.log("else");
     return await pool.query(stmt);
-
   }
-}
+};
 
 export default pool;
