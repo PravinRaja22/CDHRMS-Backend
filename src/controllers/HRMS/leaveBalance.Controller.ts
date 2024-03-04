c0d0494fd12185bd4f95d6cf808843eaf15383ea
CloudDesk <cdmacdev@gmail.com>
2024-03-04 19:31:30 +0530

    leave-balance

--- src/controllers/HRMS/leaveBalance.Controller.ts
+++ src/controllers/HRMS/leaveBalance.Controller.ts
@@ -1,23 +1,28 @@
-import { leaveBalanceService } from "../../services/Hrms/leaveBalance.service.js"; 
+import { leaveBalanceService } from "../../services/Hrms/leaveBalance.service.js";
 
-export async function getLeaveBalanceByUsers(request : any , reply : any){
-    try {
-        let queryParams:any = request.params.userId
-        let result = await leaveBalanceService.getLeaveBalanceByUsers(Number(queryParams))
-        reply.send(result)
-      } catch (error: any) {
-        reply.status(500).send(error.message);
-      }
+export async function getLeaveBalanceByUsers(request: any, reply: any) {
+  try {
+    let queryParams: any = request.params.userId;
+    let result = await leaveBalanceService.getLeaveBalanceByUsers(
+      Number(queryParams)
+    );
+    reply.send(result);
+  } catch (error: any) {
+    reply.status(500).send(error.message);
+  }
 }
 
-export async function upsertLeaveBalanceByUsers(request:any,reply:any){
-    try{
-        let queryParams:any = request.params.userId
-        let requestBody:any = request.body
-
-        let result = await leaveBalanceService.upsertLeaveBalanceByUsers(queryParams,requestBody)
-        reply.send(result)
-    }catch(error:any){
-        reply.status(500).send(error.message);
-    }
+export async function upsertLeaveBalanceByUsers(request: any, reply: any) {
+  try {
+    let queryParams: any = request.params.userId;
+    let requestBody: any = request.body;
+    console.log("upsertLeaveBalanceByUsers");
+    let result = await leaveBalanceService.upsertLeaveBalanceByUsers(
+      queryParams,
+      requestBody
+    );
+    reply.send(result);
+  } catch (error: any) {
+    reply.status(500).send(error.message);
+  }
 }
